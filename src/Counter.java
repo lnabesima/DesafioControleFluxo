b7614cee8903bc679e2f569ae4973de3a4c52a32
Lucas Nabesima <lucas.nabesima@gmail.com>
2024-08-30 08:15:25 -0300

    feat: implement error handling in Counter main method
    Added a try-catch block in the main method to handle
    InvalidParametersException. Updated the count method to throw an
    exception for invalid parameters and provided meaningful error messages.

--- src/Counter.java
+++ src/Counter.java
@@ -1,5 +1,14 @@
 public class Counter {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        try {
+            count(1, 2);
+        } catch (InvalidParametersException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-    public static void count(int num1, int num2) throws InvalidParametersException {}
+    public static void count(int num1, int num2) throws InvalidParametersException {
+        if (num1 < 0 || num2 < 0) throw new InvalidParametersException();
+        if (num1 > num2) throw new InvalidParametersException("O primeiro argumento deve ser maior que o segundo");
+    }
 }
