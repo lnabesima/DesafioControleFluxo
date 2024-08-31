5c7f05b07bd20de8852c44ae851189bc4fdeae90
Lucas Nabesima <lucas.nabesima@gmail.com>
2024-08-31 14:01:15 -0300

    feat: add counting logic
    Implemented a for loop within the count method to print numbers from 1
    to the difference between num1 and num2.

--- src/Counter.java
+++ src/Counter.java
@@ -1,7 +1,7 @@
 public class Counter {
     public static void main(String[] args) {
         try {
-            count(1, 2);
+            count(1, 500);
         } catch (InvalidParametersException e) {
             System.out.println(e.getMessage());
         }
@@ -10,5 +10,11 @@ public class Counter {
     public static void count(int num1, int num2) throws InvalidParametersException {
         if (num1 < 0 || num2 < 0) throw new InvalidParametersException();
         if (num1 > num2) throw new InvalidParametersException("O primeiro argumento deve ser maior que o segundo");
+
+        int counter = num2 - num1;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.println(i);
+        }
     }
 }
