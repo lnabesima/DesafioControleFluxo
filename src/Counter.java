dff115f1fe27ffa4dba8ceda947c0b9b9e9bb89d
Lucas Nabesima <lucas.nabesima@gmail.com>
2024-08-31 21:49:19 -0300

    feat: add input validation and loop for number entry.
    Introduced a loop structure to continuously prompt user input until
    valid numbers are given. Added handling for NumberFormatException to
    notify users when inputs are out of processable range, ensuring
    robustness.

--- src/Counter.java
+++ src/Counter.java
@@ -2,24 +2,28 @@ import java.util.Scanner;
 
 public class Counter {
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Insira dois números e a aplicação imprimirá a diferença entre eles, linha a linha.");
-            System.out.print("Insira o primeiro número: ");
-            int num1 = Integer.parseInt(scanner.nextLine());
-            System.out.print("Insira o segundo número: ");
-            int num2 = Integer.parseInt(scanner.nextLine());
+        while (true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Insira dois números e a aplicação imprimirá a diferença entre eles, linha a linha.");
+                System.out.print("Insira o primeiro número: ");
+                int num1 = Integer.parseInt(scanner.nextLine());
+                System.out.print("Insira o segundo número: ");
+                int num2 = Integer.parseInt(scanner.nextLine());
 
-            count(num1, num2);
-
-        } catch (InvalidParametersException e) {
-            System.out.println(e.getMessage());
+                count(num1, num2);
+                break;
+            } catch (InvalidParametersException e) {
+                System.out.println(e.getMessage());
+            } catch (NumberFormatException e) {
+                System.out.println("Um dos números informados estão acima da capacidade de processamento desta aplicação. Por favor, tente novamente com um número menor.");
+            }
         }
     }
 
     public static void count(int num1, int num2) throws InvalidParametersException {
         if (num1 < 0 || num2 < 0) throw new InvalidParametersException();
-        if (num1 > num2) throw new InvalidParametersException("O primeiro argumento deve ser maior que o segundo");
+        if (num1 > num2) throw new InvalidParametersException("O primeiro argumento deve ser maior que o segundo.");
 
         int counter = num2 - num1;
         System.out.printf("Vou imprimir todos os números de 1 até %d.%n", counter);
